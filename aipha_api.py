e460d9074a65e02bc302299cfdae13aa4628f0b7
Timo Hackel <timo.hackel@aipha.ch>
2022-07-23 18:18:48 +0200

    update of api definitions

--- aipha_api.py
+++ aipha_api.py
@@ -6,8 +6,34 @@ import ssl
 
 verifySSL = False #TODO: verifySSL=True
 
+def check_command_arguments(
+        command, 
+        parameters, 
+        command_dict
+    ):
+    if not command in command_dict:
+      raise RuntimeError("Invalid command request: " + command + " does not exist")
+    valid_parameters = dict(**(command_dict[command]['in_parameters']), **(command_dict[command]['out_parameters']))
+    all_parameters = {}
+    image_name = command_dict[command]['image']
+    instance_parameters = {}
+    if 'instance_type' in command_dict[command]:
+        instance_parameters['instance_type'] = command_dict[command]['instance_type']['default_value']
+    for parameter in valid_parameters:
+        if parameter in parameters:
+            all_parameters[parameter] = parameters[parameter]
+        elif 'parameter' == instance_type:
+            instance_parameteters['instance_type'] = parameters[parameter]
+        else:
+            all_parameters[parameter] = valid_parameters[parameter]['default_value']
+    return all_parameters, instance_parameters, image_name
+
+glob_commands = {}
 def import_commands(server_address):
     url = "https://" + server_address + "/default_functions.json"
+    global glob_commands
+    if glob_commands != {}:
+        return glob_commands
     if not verifySSL:
       ctx = ssl.create_default_context()
       ctx.check_hostname = False
@@ -16,25 +42,8 @@ def import_commands(server_address):
     else:
       commands_string = urllib.request.urlopen(url).read()
     commands = json.loads(commands_string)
+    glob_commands = commands
     return commands
-
-def check_command_arguments(
-        command, 
-        parameters, 
-        command_dict
-    ):
-    if not command in command_dict:
-      raise RuntimeError("Invalid command request: " + command + " does not exist")
-    print(command_dict[command]['in_parameters'], command_dict[command]['out_parameters'])
-    valid_parameters = dict(**(command_dict[command]['in_parameters']), **(command_dict[command]['out_parameters']))
-    all_parameters = {}
-    image_name = command_dict[command]['image']
-    for parameter in valid_parameters:
-        if parameter in parameters:
-            all_parameters[parameter] = parameters[parameter]
-        else:
-            all_parameters[parameter] = valid_parameters[parameter]['default_value']
-    return all_parameters, image_name
 
 def command_request(
         username,
@@ -43,26 +52,35 @@ def command_request(
         parameters_dictionary,
         server_address):
   available_commands = import_commands(server_address)
-  all_parameters, image_name = check_command_arguments(command, parameters_dictionary, available_commands)
-  print(all_parameters)
-  payload = {'customerId': username, 'customerPassword': password, 'command': image_name, 'parameters': all_parameters, 'operator_name': command}
+  all_parameters, instance_parameters, image_name = check_command_arguments(command, parameters_dictionary, available_commands)
+  payload = { \
+          'customerId': username, \
+          'customerPassword': password, \
+          'command': image_name, \
+          'parameters': all_parameters, \
+          'operator_name': command, \
+          'instance_parameters': instance_parameters \
+            }
   url = 'https://' + server_address +':443/run-operator'
   r = requests.post(url, json=payload, verify=verifySSL)
-  print(r.text)
-  return r
+  result = json.loads(r.text)
+  if 'error' in result:
+      raise RuntimeError('Error: ' + result['error'])
+  return result
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('--username', type=str, default="cus_KirpTL8mvhAHRB", help='input folder')
+  parser.add_argument('--username', type=str, default="", help='input folder')
   parser.add_argument('--password', type=str, default = "", help='input folder')
-  parser.add_argument('--command', type=str, default = "hello-world", help='output folder')
-  parser.add_argument('--parameters_dictionary', type=str, default = "", help='command parameters')
-  parser.add_argument('--server_address', type=str, default = "3.122.238.243", help='Server address')
+  parser.add_argument('--command', type=str, default = "hello world", help='output folder')
+  parser.add_argument('--parameters_dictionary_str', type=str, default = '{"instance_type": "nano"}', help='command parameters as string in json format')
+  parser.add_argument('--server_address', type=str, default = "18.156.36.243", help='Server address')
   args = parser.parse_args()
 
+  parameters_dictionary = json.loads(args.parameters_dictionary_str)
   command_request(
         args.username,
         args.password,
         args.command,
-        args.parameters_dictionary,
+        parameters_dictionary,
         args.server_address)
